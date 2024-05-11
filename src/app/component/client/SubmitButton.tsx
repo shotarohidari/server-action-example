d33a544cf41e057193dba4768c8e168c5b643735
左翔太朗 <s.hidaridesu@gmail.com>
2024-05-11 12:19:21 +0900

    フォームの送信中は送信ボタンを押せないように

--- src/app/component/client/SubmitButton.tsx
+++ src/app/component/client/SubmitButton.tsx
@@ -1,5 +1,8 @@
+import { useFormStatus } from "react-dom";
+
 export function SubmitButton() {
+  const { pending } = useFormStatus();
   return (
-    <input type="submit" value="送信" />
+    <input type="submit" value="送信" disabled={pending}/>
   )
 }
