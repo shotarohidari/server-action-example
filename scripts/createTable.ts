91271ebd881623ce3bd8adf8d4e12ad15ffb5eb5
左翔太朗 <s.hidaridesu@gmail.com>
2024-05-11 11:35:19 +0900

    テーブルのスキーマの変更

--- scripts/createTable.ts
+++ scripts/createTable.ts
@@ -4,7 +4,7 @@ import { sql, createClient } from "@vercel/postgres"
   try {
     await client.connect()
     const result =
-      await sql`CREATE TABLE IF NOT EXISTS Notes ( Situation varchar(255), Feeling varchar(255), Distortion varchar(255), Evidence varchar(255), Disproof varchar(255), Restructuring varchar(255), Change varchar(255));`
+      await sql`CREATE TABLE IF NOT EXISTS Notes ( Situation varchar(255), Feeling varchar(255), Distortion varchar(255), Evidence varchar(255), Disproof varchar(255), Restructuring varchar(255), Change varchar(255), Time timestamp);`
     console.dir(result, { depth: null })
   } catch (e) {
     console.error(e)
