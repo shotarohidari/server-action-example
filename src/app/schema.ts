1e5098dd969986c11215d8bcb83ca483e61439aa
左翔太朗 <s.hidaridesu@gmail.com>
2024-05-12 08:14:29 +0900

    スキーマの修正

--- src/app/schema.ts
+++ src/app/schema.ts
@@ -1,27 +1,16 @@
 import {
   string,
   maxLength,
-  isoTimestamp,
   object,
   minLength,
   safeParse,
   Output,
   special,
 } from "valibot"
-import { isTimeStamp } from "./util"
 
 export function validateNote(value: unknown) {
   return safeParse(note, value)
 }
-
-const timestamp =
-  special<`${string}-${string}${string} ${string}:${string}:${string}`>(
-    (val) => {
-      const isValid = isTimeStamp(val)
-      console.log({ isValid })
-      return isValid
-    }
-  )
 
 const note = object({
   situation: string([minLength(1), maxLength(255)]),
@@ -31,7 +20,6 @@ const note = object({
   disproof: string([minLength(1), maxLength(255)]),
   restructuring: string([minLength(1), maxLength(255)]),
   change: string([minLength(1), maxLength(255)]),
-  time: timestamp,
 })
 
 export type Note = Output<typeof note>
