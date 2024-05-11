2217c969ce95ff834e1de36f7c84dba5bc793124
左翔太朗 <s.hidaridesu@gmail.com>
2024-05-11 13:23:23 +0900

    SQLコマンドのAPIの修正

--- src/app/sql.ts
+++ src/app/sql.ts
@@ -1,8 +1,7 @@
-import { createClient, sql } from "@vercel/postgres"
+import { sql } from "@vercel/postgres"
 import { Note } from "./schema"
 
 export async function insertNote(note: Note) {
-  const client = createClient()
   try {
     const {
       situation,
@@ -14,12 +13,9 @@ export async function insertNote(note: Note) {
       change,
       time,
     } = note
-    const client = createClient()
     await sql`INSERT INTO Notes VALUES ('${situation}','${feeling}','${distortion}','${evidence}','${disproof}','${restructuring}','${change}','${time}')`
   } catch (e) {
     console.error(e)
     throw new Error("insert data failed.")
-  } finally {
-    await client.end()
   }
 }
