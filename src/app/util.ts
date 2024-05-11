d58ebbceffef735344ba4a44e1ffa8b45330becc
左翔太朗 <s.hidaridesu@gmail.com>
2024-05-11 13:22:56 +0900

    タイムスタンプに変換する関数の修正

--- src/app/util.ts
+++ src/app/util.ts
@@ -4,7 +4,13 @@ export function localString2Timestamp(localeString: string) {
   const match = localeString.match(regex)
   if (!match || !match.groups) return
   const { month, day, year, hour, minute, second } = match.groups
-  return `${year}-${month}-${day} ${hour}:${minute}:${second}`
+  return `${year}-${String(month).padStart(2, "0")}-${String(day).padStart(
+    2,
+    "0"
+  )} ${String(hour).padStart(2, "0")}:${String(minute).padStart(
+    2,
+    "0"
+  )}:${String(second).padStart(2, "0")}`
 }
 
 export function isTimeStamp(value: unknown) {
