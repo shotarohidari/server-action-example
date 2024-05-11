d7b5bb33384f316179ef01881661066b76899e2e
左翔太朗 <s.hidaridesu@gmail.com>
2024-05-11 09:43:14 +0900

    テーブルを作成する処理の追加

---
+++ scripts/createTable.ts
@@ -0,0 +1,14 @@
+import { sql, createClient } from "@vercel/postgres"
+;(async () => {
+  const client = createClient()
+  try {
+    await client.connect()
+    const result =
+      await sql`CREATE TABLE IF NOT EXISTS Notes ( Situation varchar(255), Feeling varchar(255), Distortion varchar(255), Evidence varchar(255), Disproof varchar(255), Restructuring varchar(255), Change varchar(255));`
+    console.dir(result, { depth: null })
+  } catch (e) {
+    console.error(e)
+  } finally {
+    await client.end()
+  }
+})()
