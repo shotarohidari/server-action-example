e740cc7a86942c42e68acd9a58c7975ea30d22dd
左翔太朗 <s.hidaridesu@gmail.com>
2024-05-11 11:47:29 +0900

    フィールドの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import { Stack, Field } from "./component"
+import { Stack, NoteField } from "./component"
 import { SubmitButton } from "./component/client"
 export default function Home() {
   return (
     <form>
       <Stack>
-        <Field label="状況" name="situation" />
-        <Field label="気分" name="feeling" />
-        <Field label="自動思考" name="distortion" />
-        <Field label="根拠" name="evidence" />
-        <Field label="反証" name="disproof" />
-        <Field label="適応的思考" name="restructuring" />
-        <Field label="心の変化" name="change" />
+        <NoteField label="状況" name="situation" />
+        <NoteField label="気分" name="feeling" />
+        <NoteField label="自動思考" name="distortion" />
+        <NoteField label="根拠" name="evidence" />
+        <NoteField label="反証" name="disproof" />
+        <NoteField label="適応的思考" name="restructuring" />
+        <NoteField label="心の変化" name="change" />
         <SubmitButton />
       </Stack>
     </form>
