b7c77bfd02339ea1f7b54a78693f73a3d74908a0
左翔太朗 <s.hidaridesu@gmail.com>
2024-05-12 09:07:08 +0900

    投稿を表示するページの追加

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,36 @@
+import { Stack } from "../component"
+import { Card } from "../component/Card"
+import { DateInfo } from "../component/DateInfo"
+import { fetchNote } from "../dbAPI"
+import { keytoLabel } from "../util"
+
+export default async function Page(props: { params: { id: string } }) {
+  const { id } = props.params
+
+  const numId = Number(id)
+  if (!Number.isInteger(numId)) {
+    throw new Error("IDは自然数で指定してください。")
+  }
+  const note = await fetchNote(numId)
+  if (!note) {
+    throw new Error("存在しないNoteです")
+  }
+  return (
+    <main>
+      <Stack>
+      <DateInfo date={note.createdAt}/>
+        {Object.entries(note)
+          .filter(([key, _]) => key !== "id" && key !== "createdAt")
+          .map(([key, value]) => {
+            return (
+              <Card
+                key={key}
+                title={keytoLabel(key)}
+                description={String(value)}
+              />
+            )
+          })}
+      </Stack>
+    </main>
+  )
+}
